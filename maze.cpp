8eaafb64d0c0e254ec7b55692b9ce5788ef583ab
Phạm Nguyễn Thu Giang <24022784@vnu.edu.vn>
2025-02-26 13:09:27 +0700

    update

--- maze.cpp
+++ maze.cpp
@@ -1,12 +1,12 @@
 #include <SDL.h>
 #include <iostream>
 
-const int SCREEN_WIDTH = 400;
+const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
-const int TILE_SIZE = 40;  // Each tile is 40x40 pixels
+const char* TITLE="Maze Escape";
+const int TILE_SIZE = 60;
 const int MAP_ROWS = 10;
 const int MAP_COLS = 10;
-const char* TITLE="Maze Escape";
 
 void initSDL(SDL_Window* &window, SDL_Renderer* &renderer){
     window = SDL_CreateWindow(TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
@@ -14,9 +14,9 @@ void initSDL(SDL_Window* &window, SDL_Renderer* &renderer){
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
     SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
 }
-// Maze representation: 1 = Wall, 0 = Path
+
 int maze[MAP_ROWS][MAP_COLS] = {
-    {1,1,1,1,1,1,1,1,1,1},
+    {0,0,1,1,1,1,1,1,1,1},
     {1,0,0,0,0,1,0,0,0,1},
     {1,0,1,1,0,1,0,1,0,1},
     {1,0,1,0,0,0,0,1,0,1},
@@ -24,12 +24,12 @@ int maze[MAP_ROWS][MAP_COLS] = {
     {1,0,1,0,1,0,0,1,0,1},
     {1,0,1,0,1,0,1,1,0,1},
     {1,0,0,0,0,0,0,0,0,1},
-    {1,1,1,1,1,1,1,1,1,1}
+    {1,1,1,1,1,1,1,1,0,0}
 };
 
-// Function to draw the maze
+
 void drawMaze(SDL_Renderer* renderer) {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);  // Blue color for walls
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);  // Blue color
 
     for (int row = 0; row < MAP_ROWS; row++) {
         for (int col = 0; col < MAP_COLS; col++) {
@@ -64,17 +64,12 @@ int main(int argc, char* argv[]) {
         SDL_Renderer* renderer;
         initSDL(window, renderer);
 
-            // Clear the screen
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);  // Black background
         SDL_RenderClear(renderer);
-
-        // Draw the maze
         drawMaze(renderer);
-
         SDL_RenderPresent(renderer);
-    waitUntilKeyPressed();
-    quitSDL(window, renderer);
-
+        waitUntilKeyPressed();
+        quitSDL(window, renderer);
 
     return 0;
 }
