c5200f47f911f7740ff74aff9a446a4865ba6cd1
Phạm Nguyễn Thu Giang <24022784@vnu.edu.vn>
2025-05-03 23:26:47 +0700

    update

--- Doodle Jump/GameOver.h
+++ Doodle Jump/GameOver.h
@@ -3,6 +3,23 @@
 
 #include <SDL.h>
 #include <string>
+#include <vector>
+
+enum class ButtonType {PlayAgain, Menu};
+
+class Button {
+public:
+    Button (const SDL_FRect &box, SDL_Texture* texture, const SDL_Rect& spriteCut, ButtonType type);
+    void draw(SDL_Renderer *renderer);
+    bool contains(int x, int y) const;
+    ButtonType getType() const;
+
+private:
+    SDL_FRect box_;
+    SDL_Texture* texture_;
+    SDL_Rect spriteCut_;
+    ButtonType type_;
+};
 
 class GameOverScreen {
 public:
@@ -12,6 +29,7 @@ public:
     void render(SDL_Renderer* render);
     bool isGameOver() const;
     void setGameOver(bool over);
+    Button* checkClick (int mx, int my);
 private:
     bool gameOver;
     SDL_Texture* gameOverTexture;
